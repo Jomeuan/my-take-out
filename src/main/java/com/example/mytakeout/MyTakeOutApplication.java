7c3ee59bd1fc596b031af0031b8294f5564cda87
Jomeuan Ma <ma0zzzhyr@gmail.com>
2024-03-21 20:27:05 +0800

    第五次？

--- src/main/java/com/example/mytakeout/MyTakeOutApplication.java
+++ src/main/java/com/example/mytakeout/MyTakeOutApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //TODO: 编写全局的我的配置信息类
-
-
+//第二次push？
+//第三次用终端的git试试
 @SpringBootApplication
 public class MyTakeOutApplication {
 
