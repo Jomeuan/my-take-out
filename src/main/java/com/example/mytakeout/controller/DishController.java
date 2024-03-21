62e23c4fe6608a58a981684cebc142baa6c5efbc
Jomeuan Ma <ma0zzzhyr@gmail.com>
2024-03-21 20:56:53 +0800

    第六次

--- src/main/java/com/example/mytakeout/controller/DishController.java
+++ src/main/java/com/example/mytakeout/controller/DishController.java
@@ -11,8 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
